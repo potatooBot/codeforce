604d02159ee347d13f32cb9ab474b37551cbf3f7
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-05-07 22:23:31 +0530

    Committing files

---
+++ CodeForces/Java 8/4A | Watermelon/184330689.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+public class alice {
+ 
+public static void watermelon(){
+    Scanner sc=new Scanner(System.in);
+    int w=sc.nextInt();
+    if(w%2==0&&w!=2){
+ 
+        System.out.println("YES");
+        return;
+    }
+    System.out.println("NO");
+}
+ 
+    public static void main(String[] args) {
+ 
+      watermelon();
+    }
+}
+ 
+ 
+ 
